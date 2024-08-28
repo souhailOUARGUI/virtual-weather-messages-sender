7e91fc2f133d264a47b2758354fee3ffc0c81ef1
souhailOUARGUI <ouargui550@gmail.com>
2024-08-28 16:42:36 +0100

    virtual sender updated

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-// const cors = require("cors");
 const env = require("dotenv").config();
 const bodyParser = require("body-parser");
 const { sendMetar, sendSynop } = require("./virtualSender");
@@ -13,10 +12,10 @@ app.use(express.json());
 // app.use(cors);
 app.listen(port, () => {
   console.log(`VSender server started!, listening on port ${port}`);
-  setInterval(() => {
-    sendSynop();
-  }, 1000 * 60 * 60);
+  // setInterval(() => {
+  //   sendSynop();
+  // }, 1000 * 60 * 60);
   setInterval(() => {
     sendMetar();
-  }, 1000 * 60 * 60);
+  }, 5000);
 });
