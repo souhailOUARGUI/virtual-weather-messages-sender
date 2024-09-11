dff659ca9f23c32ed7dbaeeb1881e3e98c5ae39b
souhailOUARGUI <ouargui550@gmail.com>
2024-09-11 17:03:17 +0100

    solved Render spin down problem

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const env = require("dotenv").config();
 const bodyParser = require("body-parser");
 const { sendMetar, sendSynop } = require("./virtualSender");
 
+const render_url = `https://moga-weather-api.onrender.com`; //Render URL
+const render_interval = 60000; // Interval in milliseconds (60 seconds)
+
 const app = express();
 const port = 4000;
 
@@ -17,5 +20,27 @@ app.listen(port, () => {
   // }, 1000 * 60 * 60);
   setInterval(() => {
     sendMetar();
-  }, 1000 * 60 * 5);
+    // sendSynop();
+  }, 4000);
+
+  setInterval(reloadWebsite, render_interval);
 });
+
+//Reloader Function
+function reloadWebsite() {
+  axios
+    .get(render_url)
+    .then((response) => {
+      console.log(
+        `Reloaded at ${new Date().toISOString()}: Status Code ${
+          response.status
+        }`
+      );
+    })
+    .catch((error) => {
+      console.error(
+        `Error reloading at ${new Date().toISOString()}:`,
+        error.message
+      );
+    });
+}
